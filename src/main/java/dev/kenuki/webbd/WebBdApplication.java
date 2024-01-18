488963328cc086dbcfeeb82e448eb3c26800f6a1
TheJustRusik <7thejustrusik7@gmail.com>
2024-01-19 02:01:56 +0600

    base functional for working with DataBase

--- src/main/java/dev/kenuki/webbd/WebBdApplication.java
+++ src/main/java/dev/kenuki/webbd/WebBdApplication.java
@@ -1,13 +1,28 @@
 package dev.kenuki.webbd;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class WebBdApplication {
+public class WebBdApplication implements CommandLineRunner {
+    @Autowired
+    private JobsRepository jobsRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(WebBdApplication.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<Jobs> jobsEntities = jobsRepository.findAll();
+        for (Jobs job : jobsEntities){
+            System.out.println(job.getJob_id() + " " + job.getJob_title() + " " + job.getMin_salary() + " " + job.getMax_salary());
+        }
+
     }
 
 }
