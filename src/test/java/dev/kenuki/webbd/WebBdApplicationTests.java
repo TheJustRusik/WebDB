258e25ff18f1413373e832db23482a5732c3c105
TheJustRusik <7thejustrusik7@gmail.com>
2024-01-19 00:06:20 +0600

    Initial commit

---
+++ src/test/java/dev/kenuki/webbd/WebBdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kenuki.webbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
